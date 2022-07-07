4620b682ba6c52bc7a08b527da7f8e408e042183
Macc <2453170895@qq.com>
2022-07-07 10:27:40 +0800

    定时器bug和左按钮切换bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
   const arrowPrev = document.querySelector('.banner .arrows .left')
   const arrowNext = document.querySelector('.banner .arrows .right')
   const btns = document.querySelectorAll('.banner .btns .btn-item')
-  let index = 0
+  let index = 0, timer
 
   btns.forEach((item, idx) => {
     item.addEventListener('click', () => {
@@ -34,24 +34,24 @@
       index = 3
       container.style.left = -index * 600 + 'px'
     } else {
-      container.style.left = index * 600 + 'px'
+      container.style.left = -index * 600 + 'px'
     }
     changeCurrentBtn()
   })
 
   document.querySelector('.banner').addEventListener('mouseover', () => {
     console.log(1)
-    if(timer)
-    clearTimeout(timer)
+    if (timer)
+      clearTimeout(timer)
   })
 
-  let timer = setTimeout(function loop() {
+  timer = setTimeout(function loop() {
     arrowNext.click()
     timer = setTimeout(loop, 1000)
   }, 1000)
 
   container.addEventListener('mouseout', () => {
-    let timer = setTimeout(function loop() {
+    timer = setTimeout(function loop() {
       arrowNext.click()
       timer = setTimeout(loop, 1000)
     }, 1000)
